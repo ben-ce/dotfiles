7523a98b8d34c0c94031e52e1c3ca6671e4203e2
ben-ce <bence.schmehl@protonmail.com>
2022-02-18 14:00:06 +0100

    check running machine type for battery widget

--- .config/awesome/signals/battery.lua
+++ .config/awesome/signals/battery.lua
@@ -3,12 +3,20 @@
 -- signal::battery
 --      percentage
 --      state
-local upower_widget = require("modules.awesome-battery_widget")
-local battery_listener = upower_widget({
-	device_path = "/org/freedesktop/UPower/devices/battery_BAT0",
-	instant_update = true,
-})
+local awful = require'awful'
+local naughty = require('naughty')
 
-battery_listener:connect_signal("upower::update", function(_, device)
-	awesome.emit_signal("signal::battery", device.percentage, device.state)
+awful.spawn.easy_async('cat /sys/class/dmi/id/chassis_type', function (out)
+  -- check if the host machine is a laptop/desktop with the chassis_type value
+  if out ~= "3" then
+    local upower_widget = require("modules.awesome-battery_widget")
+    local battery_listener = upower_widget({
+    	device_path = "/org/freedesktop/UPower/devices/battery_BAT0",
+    	instant_update = true,
+    })
+
+    battery_listener:connect_signal("upower::update", function(_, device)
+    	awesome.emit_signal("signal::battery", device.percentage, device.state)
+    end)
+  end
 end)
