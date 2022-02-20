c157de9d94e823f9cbf8a8e0f29afc6f4f580919
ben-ce <bence.schmehl@protonmail.com>
2022-02-20 18:04:16 +0100

    change info and looks of battery widget

--- .config/awesome/widgets/battery.lua
+++ .config/awesome/widgets/battery.lua
@@ -1,36 +1,57 @@
+local awful = require("awful")
 local wibox = require("wibox")
 local beautiful = require("beautiful")
-local gears = require("gears")
+local apps = require("config.apps")
+local upower = require('lgi').require('UPowerGlib')
+-- This uses UPowerGlib.Device (https://lazka.github.io/pgi-docs/UPowerGlib-1.0/classes/Device.html)
+-- Provides:
+-- signal::battery
+--      percentage
+--      state
+--      time_to_empty
 
+-- awful.spawn.easy_async('cat /sys/class/dmi/id/chassis_type', function (out)
+  -- check if the host machine is a laptop/desktop with the chassis_type value
+  -- if out ~= "3" then
+    local upower_widget = require("modules.awesome-battery_widget")
+    local battery_listener = upower_widget({
+      instant_update = true,
+      use_display_device = true,
+    })
+
+    battery_listener:connect_signal("upower::update", function(_, device)
+      awesome.emit_signal("signal::battery", device.percentage, device.state, device.time_to_empty, device.time_to_full)
+    end)
+  -- end
+-- end)
 
 local battery_icon = wibox.widget({
-  font = beautiful.font,
 	align = "center",
 	valign = "center",
+  resize = true,
 	widget = wibox.widget.textbox,
 })
 
-local battery_prog = wibox.widget({
-	max_value = 100,
-	min_value = 0,
-	value = 20,
-	forced_height = 3,
-	forced_width = 75,
-	color = beautiful.red,
-	background_color = beautiful.primary,
-	shape = gears.shape.rounded_bar,
-	widget = wibox.widget.progressbar,
+local battery_tooltip = awful.tooltip({
+  objects = {battery_icon},
+  mode = "outside",
+  align = "left",
+  margin = 10,
+  referred_positions = {"right", "left", "top", "bottom"}
 })
 
-awesome.connect_signal("signal::battery", function(percentage, state)
+awesome.connect_signal("signal::battery", function(percentage, state, time_to_empty, time_to_full)
 	local value = percentage
 
 	local bat_icon = "  "
-	local bat_color = beautiful.green
-  battery_icon.font = "MesloLGS Nerd Font 15"
+	local bat_color = beautiful.white
+  battery_icon.font = "JetBrains Mono Nerd Font 15"
+  local time = time_to_empty
+  local time_to_x = 'empty'
+  local percentage_font = beautiful.font_name .. "10"
 
 	if value >= 0 and value <= 15 then
-		bat_icon = "﮻ "
+		bat_icon = "  "
 		bat_color = beautiful.red
 	elseif value > 15 and value <= 20 then
 		bat_icon = "  "
@@ -58,19 +79,19 @@ awesome.connect_signal("signal::battery", function(percentage, state)
 		bat_color = beautiful.white
 	elseif value > 90 and value <= 100 then
 		bat_icon = "  "
-		bat_color = beautiful.white
+	  bat_color = beautiful.white
 	end
 
 	-- if charging
 	if state == 1 then
-    battery_icon.font = beautiful.icon_font .. " " .. beautiful.icon_size
-		bat_icon = " "
+    battery_icon.font = beautiful.icon_font
+		bat_icon = ""
 		bat_color = beautiful.blue
+    time = time_to_full
+    time_to_x = 'full'
 	end
-
-	battery_prog.value = percentage
-
-	battery_icon.markup = "<span foreground='" .. bat_color .. "'>" .. bat_icon .. "</span>"
+	battery_icon.markup = "<span font='" .. percentage_font .. "' rise='3pt'>" .. string.format('%3d', percentage) .. "% " .. "</span><span foreground='" .. bat_color .. "'>"  .. bat_icon .. "</span>"
+  battery_tooltip.text = string.format('State: %s \nPercentage: %3d%% \nTime to %s: %s', upower.DeviceState[state], percentage, time_to_x, upower_widget.to_clock(time))
 end)
 
 return battery_icon
