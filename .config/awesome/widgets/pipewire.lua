57487c1102393ca46c385eaae0753e872d561772
ben-ce <bence.schmehl@protonmail.com>
2022-09-29 18:47:17 +0200

    make volume widget work like the brightness widget

--- .config/awesome/widgets/pipewire.lua
+++ .config/awesome/widgets/pipewire.lua
@@ -1,6 +1,12 @@
+-- __     __    _
+-- \ \   / /__ | |_   _ _ __ ___   ___
+--  \ \ / / _ \| | | | | '_ ` _ \ / _ \
+--   \ V / (_) | | |_| | | | | | |  __/
+--    \_/ \___/|_|\__,_|_| |_| |_|\___|
+--
+
 -------------------------------------------------
 -- Speaker Volume Widget for Pipewire-Pulse
--- Also toggles noisetorch
 -------------------------------------------------
 
 local awful = require("awful")
@@ -9,24 +15,30 @@ local watch = require("awful.widget.watch")
 local beautiful = require("beautiful")
 local wbutton = require("widgets.button")
 
-local GET_SINK_VOL_CMD = "pactl get-sink-volume @DEFAULT_SINK@"
-local GET_SINK_MUTE_CMD = "pactl get-sink-mute @DEFAULT_SINK@"
-local GET_SOURCE_VOL_CMD = "pactl get-source-volume @DEFAULT_SOURCE@"
--- local NOISETORCH_CMD = "pactl get-source-volume \'NoiseTorch Microphone\'"
-
-local UPDATE_CMD = string.format("bash -c \"%s && %s\"", GET_SINK_VOL_CMD, GET_SINK_MUTE_CMD)
+local get_volume_cmd
+local inc_volume_cmd
+local dec_volume_cmd
+local mute_volume_cmd
 
 local widget = {}
 
-local worker = function(user_args)
-
+local function worker(user_args)
   local args = user_args or {}
 
-  local icon = args.icon;
-  local font = beautiful.font
-  local timeout = 2;
+  local icon = args.icon
+  local font = args.font or beautiful.font
+  local timeout = args.timeout or 2
+  local program = args.program or 'pactl'
 
-  local volume = wibox.widget{
+  if program == 'pactl' then
+    get_volume_cmd = 'bash -c "pactl get-sink-volume @DEFAULT_SINK@ && pactl get-sink-mute @DEFAULT_SINK@"'
+    inc_volume_cmd = 'pactl set-sink-volume @DEFAULT_SINK@ +5%'
+    dec_volume_cmd = 'pactl set-sink-volume @DEFAULT_SINK@ -5%'
+    mute_volume_cmd = 'pactl set-sink-mute @DEFAULT_SINK@ toggle'
+  end
+
+  local volume = {}
+  volume.widget = wibox.widget{
     layout = wibox.layout.fixed.horizontal,
     spacing = 4,
     {
@@ -36,7 +48,6 @@ local worker = function(user_args)
       resize = true,
       font = "Iosevka 24",
       widget = wibox.widget.textbox,
-      text = '墳'
     },
     {
       id = "volume",
@@ -46,61 +57,94 @@ local worker = function(user_args)
 
     update_volume = function(self, volume, is_mute)
       local font_color = beautiful.fg_normal
-      local volume_text = string.format("%s", volume);
+      local volume_icon
+      local volume_text = string.format("%s", volume)
       if is_mute then
+        volume_icon = string.format("%s", '\u{fc5d}')
         volume_text = string.format("%s", "mute")
-        font_color = beautiful.fg_urgent
+        font_color = beautiful.gray
+      else
+        local vol_value = volume_text:gsub('%%','')
+        if tonumber(vol_value) >= 50 then
+          volume_icon = string.format("%s", '\u{f028}')
+        elseif tonumber(vol_value) >= 25 then
+          volume_icon = string.format("%s", '\u{f027}')
+        else
+          volume_icon = string.format("%s", '\u{f026}')
+        end
       end
-      local volume_markup = string.format("<span font='%s' foreground='%s'>%s</span>", font, font_color, volume_text);
+      local volume_icon_markup = string.format("<span font='%s' foreground='%s'>%s</span>", self.icon.font, font_color, volume_icon)
+      local volume_markup = string.format("<span font='%s' foreground='%s'>%s</span>", font, font_color, volume_text)
 
+      if self.icon:get_markup() ~= volume_icon_markup then
+        self.icon:set_markup(volume_icon_markup)
+      end
       if self.volume:get_markup() ~= volume_markup then
-        self.volume:set_markup(volume_markup);
+        self.volume:set_markup(volume_markup)
       end
     end
   }
 
-  local update_widget = function(widget, stdout, _, _, _)
-    local vol_left, vol_right = string.match(stdout, "/ *(%S+)");
+  local update_widget = function(volume, stdout, _, _, _)
+    local vol_left, vol_right = string.match(stdout, "/ *(%S+)")
     -- if album ~= nil and title ~= nil and artist ~= nil then
     local is_mute = string.match(stdout, "Mute: yes") or nil
-    widget:update_volume(vol_left, is_mute);
-    -- end;
-  end;
+    volume:update_volume(vol_left, is_mute)
+    -- end
+  end
 
-  watch(UPDATE_CMD, timeout, update_widget, volume);
+  function volume:mute()
+    awful.spawn.easy_async(mute_volume_cmd, function ()
+      awful.spawn.easy_async(get_volume_cmd, function(stdout, stderr, _, _)
+        update_widget(volume.widget, stdout, stderr)
+      end)
+    end)
+  end
+
+  function volume:inc()
+    awful.spawn.easy_async(inc_volume_cmd, function ()
+      awful.spawn.easy_async(get_volume_cmd, function(stdout, stderr, _, _)
+        update_widget(volume.widget, stdout, stderr)
+      end)
+    end)
+  end
+
+  function volume:dec()
+    awful.spawn.easy_async(dec_volume_cmd, function ()
+      awful.spawn.easy_async(get_volume_cmd, function(stdout, stderr, _, _)
+        update_widget(volume.widget, stdout, stderr)
+      end)
+    end)
+  end
+
+  function volume:open()
+      awful.spawn("pavucontrol");
+  end
 
   --- Adds mouse controls to the widget:
   --  - left click - pavucontrol
   --  - scroll up - volume up
   --  - scroll down - volume down
   --  - right click - start noisetorch
-  volume:connect_signal("button::press", function(_, _, _, button)
-    if button == 3 then
-      awful.spawn("pavucontrol");
-      return
-      -- using amixer instead of pactl to limit volume to 100%
-    elseif button == 4 then
-      awful.spawn("pactl set-sink-volume @DEFAULT_SINK@ +5%", false);
-      -- awful.spawn("amixer set Master 5%+", false);
-    elseif button == 5 then
-      awful.spawn("pactl set-sink-volume @DEFAULT_SINK@ -5%", false);
-      -- awful.spawn("amixer set Master 5%-", false);
-    elseif button == 1 then
-      awful.spawn("pactl set-sink-mute @DEFAULT_SINK@ toggle", false);
-    end;
-    awful.spawn.easy_async(UPDATE_CMD, function(stdout, stderr, _, _) update_widget(volume, stdout, stderr) end)
-  end
-  );
+  volume.widget:buttons(
+    awful.util.table.join(
+      awful.button({}, 1, function() volume:mute() end),
+      awful.button({}, 3, function() volume:open() end),
+      awful.button({}, 4, function() volume:inc() end),
+      awful.button({}, 5, function() volume:dec() end)
+    )
+  )
+  watch(get_volume_cmd, timeout, update_widget, volume.widget)
 
   widget = wbutton.elevated.state({
-    child = volume,
+    child = volume.widget,
     normal_bg = beautiful.bg_normal,
   })
-
   return widget
-end;
-
-return setmetatable(widget, {	__call = function(_, ...)
-  return worker(...);
 end
-});
+
+return setmetatable(widget, {
+  __call = function(_, ...)
+  return worker(...)
+  end
+})
