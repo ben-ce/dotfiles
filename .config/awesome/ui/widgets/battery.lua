a039c5a4eeef3d06aa6928530df3650e6b2f5ff9
ben-ce <bence.schmehl@protonmail.com>
2022-11-16 15:26:14 +0100

    [ui] create battery progressbar
    [old] rename unused battery widget to icon_text_battery.lua

--- .config/awesome/ui/widgets/battery.lua
+++ .config/awesome/ui/widgets/battery.lua
@@ -1,121 +1,157 @@
 local awful = require("awful")
 local wibox = require("wibox")
 local beautiful = require("beautiful")
+-- require("signal.battery")
+local gears = require("gears")
 local apps = require("config.apps")
+local dpi = require("beautiful").xresources.apply_dpi
+local helpers = require("helpers")
 local upower = require('lgi').require('UPowerGlib')
 local wbutton = require("ui.widgets.button")
 
--- This uses UPowerGlib.Device (https://lazka.github.io/pgi-docs/UPowerGlib-1.0/classes/Device.html)
--- Provides:
--- signal::battery
---      percentage
---      state
---      time_to_empty
---      time_to_full
---      kind
+--- Battery Widget
+--- ~~~~~~~~~~~~~~
 local upower_widget = require("modules.awesome-battery_widget")
 local battery_listener = upower_widget({
-  instant_update = true,
+	instant_update = true,
   use_display_device = true,
 })
 
 battery_listener:connect_signal("upower::update", function(_, device)
   awesome.emit_signal("signal::battery", device.percentage, device.state, device.time_to_empty, device.time_to_full, device.kind)
 end)
---
 
+return function()
+	local happy_color = beautiful.fg_normal
+	-- local ok_color = beautiful.color2
+  local warn_color = beautiful.orange
+	local sad_color = beautiful.red
+	local charging_color = beautiful.teal
 
-local battery = wibox.widget{
-  layout = wibox.layout.fixed.horizontal,
-  spacing = 4,
-  {
-    id = 'icon',
-    align = "center",
-    valign = "center",
-    resize = true,
-    font = "Iosevka 40",
-    widget = wibox.widget.textbox,
-  },
-  {
-    id = 'value',
-    align = "center",
-    valign = "center",
-    resize = true,
-    widget = wibox.widget.textbox,
+	local charging_icon = wibox.widget({
+		markup = helpers.ui.colorize_text("", beautiful.white),
+		font = beautiful.icon_font_name .. "Round 14",
+		align = "center",
+		valign = "center",
+		widget = wibox.widget.textbox,
+	})
+
+	local battery_bar = wibox.widget({
+		max_value = 100,
+		value = 50,
+		forced_width = dpi(30),
+		border_width = dpi(1),
+    -- forced_height = dpi(10),
+		paddings = dpi(1),
+		bar_shape = helpers.ui.rrect(2),
+		shape = helpers.ui.rrect(4),
+		color = beautiful.white,
+		background_color = beautiful.transparent,
+		border_color = beautiful.white .. 'BB',
+		widget = wibox.widget.progressbar,
+	})
+
+	local battery_decoration = wibox.widget({
+		{
+			wibox.widget.textbox,
+			widget = wibox.container.background,
+			bg = beautiful.white .. 'BB',
+			forced_width = dpi(7.2),
+			forced_height = dpi(7.2),
+			shape = function(cr, width, height)
+				gears.shape.pie(cr, width, height, 0, math.pi)
+			end,
+		},
+		direction = "east",
+		widget = wibox.container.rotate(),
+	})
+
+	local battery = wibox.widget({
+		charging_icon,
+		{
+			battery_bar,
+			battery_decoration,
+			layout = wibox.layout.fixed.horizontal,
+			spacing = dpi(-2.0),
+		},
+		layout = wibox.layout.fixed.horizontal,
+		spacing = dpi(1),
+	})
+
+	local battery_percentage_text = wibox.widget({
+		id = "percent_text",
+		text = "50%",
+		font = beautiful.font_name .. "Medium 12",
+		align = "center",
+		valign = "center",
+		widget = wibox.widget.textbox,
+	})
+
+	local battery_widget = wibox.widget({
+		layout = wibox.layout.fixed.horizontal,
+		spacing = dpi(5),
+    battery,
+		battery_percentage_text,
+	})
+  local battery_tooltip = awful.tooltip{
+    objects = {battery},
+    mode = "outside",
+    align = "left",
+    margin = 10,
+    preferred_positions = {"right", "left", "top", "bottom"}
   }
-}
 
-battery:connect_signal("button::press", function (_,_,_,button)
-  if button == 1 then
-    awful.spawn(apps.power_manager);
-    return
-  end
-end)
+	local widget = wbutton.elevated.state({
+		child = battery_widget,
+		normal_bg = beautiful.wibar_bg,
+		on_release = function()
+			awful.spawn(apps.power_manager, false)
+		end,
+    margins = dpi(2),
+	})
 
-local battery_tooltip = awful.tooltip{
-  objects = {battery},
-  mode = "outside",
-  align = "left",
-  margin = 10,
-  preferred_positions = {"right", "left", "top", "bottom"}
-}
+	local last_value = 100
+	awesome.connect_signal("signal::battery", function(value, state, time_to_empty, time_to_full, kind)
+		battery_bar.value = value
+		last_value = value
+    local time = time_to_empty
+    local time_to_x = 'empty'
 
+		battery_percentage_text:set_text(math.floor(value) .. "%")
 
-awesome.connect_signal("signal::battery", function(percentage, state, time_to_empty, time_to_full, kind)
-  local value = percentage
-  local bat_icon = ""
-  local bat_color = beautiful.white
-  local time = time_to_empty
-  local time_to_x = 'empty'
-
-  if kind == 0 then
-    bat_icon = ''
-    bat_color = beautiful.green
-    battery.icon.markup = string.format("<span size='28pt' foreground='%s'>%s</span>", bat_color, bat_icon)
-    battery.value.markup = string.format("%s", "N/A")
-    battery_tooltip.text = string.format('%s', "Unknown device, likely running on AC Line Power.")
-  elseif kind == 1 then
-    bat_icon = ''
-    bat_color = beautiful.blue
-    battery.icon.markup = string.format("<span size='28pt' foreground='%s'>%s</span>", bat_color, bat_icon)
-    battery.value.markup = string.format("%s", "value")
-    battery_tooltip.text = string.format('%s', "Running on AC Line Power.")
-  else
-    if value >= 0 and value <= 20 then
-      bat_icon = ""
-      bat_color = beautiful.red
-    elseif value > 20 and value <= 40 then
-      bat_icon = ""
-      bat_color = beautiful.orange
-    elseif value > 40 and value <= 60 then
-      bat_icon = ""
-      bat_color = beautiful.yellow
-    elseif value > 60 and value <= 85 then
-      bat_icon = ""
-      bat_color = beautiful.white
-    elseif value > 85 and value <= 100 then
-      bat_icon = ""
-      bat_color = beautiful.white
-    end
-
-    -- if charging
-    if state == 1 then
-      bat_icon = ''
-      bat_color = beautiful.green
-      time = time_to_full
-      time_to_x = 'full'
-      battery.icon.markup = string.format("<span size='28pt' foreground='%s'>%s</span>", bat_color, bat_icon)
+    if kind == 0 then
+      battery_tooltip.text = string.format('%s', "Unknown device, likely running on AC Line Power.")
+    elseif kind == 1 then
+      battery_tooltip.text = string.format('%s', "Running on AC Line Power.")
     else
-      battery.icon.markup = string.format("<span foreground='%s'>%s</span>", bat_color, bat_icon)
+      if charging_icon.visible then
+        battery_bar.color = charging_color
+      elseif value <= 15 then
+        battery_bar.color = sad_color
+      elseif value <= 30 then
+        battery_bar.color = warn_color
+      else
+        battery_bar.color = happy_color
+      end
+
+      if state == 1 then
+        charging_icon.visible = true
+        battery_bar.color = charging_color
+        time = time_to_full
+        time_to_x = 'full'
+      elseif last_value <= 15 then
+        charging_icon.visible = false
+        battery_bar.color = sad_color
+      elseif last_value <= 30 then
+        charging_icon.visible = false
+        battery_bar.color = warn_color
+      else
+        charging_icon.visible = false
+        battery_bar.color = happy_color
+      end
+      battery_tooltip.text = string.format("State: %s \nPercentage: %3d%% \nTime to %s: %s", upower.DeviceState[state], value, time_to_x, upower_widget.to_clock(time))
     end
-    battery.value.markup = string.format("%d%%", value)
-    battery_tooltip.text = string.format("State: %s \nPercentage: %3d%% \nTime to %s: %s", upower.DeviceState[state], percentage, time_to_x, upower_widget.to_clock(time))
-  end
-end)
+	end)
 
-local widget = wbutton.elevated.state({
-  child = battery,
-  normal_bg = beautiful.bg_normal,
-})
-
-return widget
+	return widget
+end
